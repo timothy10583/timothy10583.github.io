51c901d59502b5bc3ca2240fd652d2b972466b63
timothy10583 <timothy10583@gmail.com>
2021-08-22 15:53:15 -0400

    a lot of things

--- 404.js
+++ 404.js
@@ -23,7 +23,7 @@ document.getElementById("stwing").innerHTML = uncoolString[randomNumber]
 
 
 
-//FUCK
+//
 function playfart(){
     var audio = new Audio("sounds/Firework.ogg");
     audio.volume = 0.2
@@ -35,3 +35,16 @@ function playarg(source, volume){
     audio.volume = volume||1
     audio.play();
 }
+
+//$(function(){
+    var randx = Math.random()*2-1
+    var randy = Math.random()*2-1
+    var x = 0;
+    var y = 0;
+    setInterval(function(){
+        x += randx
+        y += randy
+        document.body.style.backgroundPosition = `${x}px ${y}px`
+        document.getElementById('404').style.backgroundPosition = `${x}px ${y}px`
+    }, 10);
+//})
